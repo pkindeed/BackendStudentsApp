3336f9195e9a0d751942bfb152aac0b533b55400
pkindeed <povilas.kimutis@gmail.com>
2020-06-16 18:07:48 +0300

    Working version
    Works with local DB

--- src/main/java/com/techprimers/mybatis/springbootmybatis/mapper/UsersMapper.java
+++ src/main/java/com/techprimers/mybatis/springbootmybatis/mapper/UsersMapper.java
@@ -8,14 +8,14 @@ import java.util.List;
 @Mapper
 public interface UsersMapper {
 
-    @Select("select * from users")
+    @Select("select * from Users")
     List<Users> findAll();
 
-    @Insert("insert into users(name,salary) values(#{name},#{salary})")
+    @Insert("insert into Users(name,salary) values(#{name},#{salary})")
     @SelectKey(statement = "SELECT LAST_INSERT_ID()", keyProperty = "id",
             before = false, resultType = Integer.class)
     void insert(Users users);
 
-    @Delete("delete from users")
+    @Delete("delete from Users")
     void delete();
 }
