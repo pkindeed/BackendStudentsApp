ed758518fc6c8b3590fec023ea27c7a418adf4e0
pkindeed <povilas.kimutis@gmail.com>
2020-07-28 22:13:20 +0300

    maybe diff bug
    maybe diff bug

--- src/main/java/com/techprimers/mybatis/springbootmybatis/mapper/UsersMapper.java
+++ src/main/java/com/techprimers/mybatis/springbootmybatis/mapper/UsersMapper.java
@@ -8,14 +8,14 @@ import java.util.List;
 @Mapper
 public interface UsersMapper {
 
-    @Select("select * from users")
+    @Select("select * from duomenys")
     List<Users> findAll();
 
-    @Insert("insert into users(name,salary) values(#{name},#{salary})")
+    @Insert("insert into duomenys(name,salary) values(#{name},#{salary})")
     @SelectKey(statement = "SELECT LAST_INSERT_ID()", keyProperty = "id",
             before = false, resultType = Integer.class)
     void insert(Users users);
 
-    @Delete("delete from users")
+    @Delete("delete from duomenys")
     void delete();
 }
