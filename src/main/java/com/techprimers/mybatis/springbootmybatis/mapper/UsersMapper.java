8e5b29d39ac3c2e5977f71d22c0ad5af4a672827
pkindeed <povilas.kimutis@gmail.com>
2020-07-28 22:18:38 +0300

    s
    s

--- src/main/java/com/techprimers/mybatis/springbootmybatis/mapper/UsersMapper.java
+++ src/main/java/com/techprimers/mybatis/springbootmybatis/mapper/UsersMapper.java
@@ -8,14 +8,14 @@ import java.util.List;
 @Mapper
 public interface UsersMapper {
 
-    @Select("select * from duomenys")
+    @Select("select * from users")
     List<Users> findAll();
 
-    @Insert("insert into duomenys(name,salary) values(#{name},#{salary})")
+    @Insert("insert into users(name,salary) values(#{name},#{salary})")
     @SelectKey(statement = "SELECT LAST_INSERT_ID()", keyProperty = "id",
             before = false, resultType = Integer.class)
     void insert(Users users);
 
-    @Delete("delete from duomenys")
+    @Delete("delete from users")
     void delete();
 }
